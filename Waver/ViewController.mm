978c953991b28990d32284db6ecbed11739a9aa2
guoliting <guoliting@didichuxing.com>
2016-07-21 17:52:04 +0800

    增加对ISCProject库里接口的调用

--- Waver/ViewController.mm
+++ Waver/ViewController.mm
@@ -11,6 +11,8 @@
 #import <AVFoundation/AVFoundation.h>
 #import "CircleProgressView.h"
 #import "KefuMsgClient.h"
+#import "KMFileManager.h"
+#import "KMMp3Player.h"
 
 static float percent = 0;
 
@@ -19,6 +21,7 @@ static float percent = 0;
 @property (nonatomic, strong) AVAudioRecorder *recorder;
 @property (nonatomic, strong) CircleProgressView *circleProgressView;
 @property (nonatomic, strong) KefuMsgClient *kefuClient;
+@property (nonatomic, strong) KMMp3Player *mp3Player;
 @property (nonatomic, copy) NSDictionary *userInfoDic;
 
 @end
@@ -34,7 +37,11 @@ static float percent = 0;
     self.view.backgroundColor = [UIColor whiteColor];
     _kefuClient = [[KefuMsgClient alloc] init];
 //    _kefuClient.delegate = self;
+    _mp3Player = [[KMMp3Player alloc] init];
     _userInfoDic = @{@"chatInfo":@{@"businessType":@1, @"cell":@18800000004, @"cityId":@1, @"message":@"", @"mid":@"462d7806-3295-4355-a719-1c4a71e0bf7b", @"msgType":@0, @"orderId":@0, @"roleType":@3, @"skillType":@"common", @"source":@-1, @"uid":@564069099110401}, @"pid":@10001};
+    
+    _circleProgressView = [[CircleProgressView alloc] initWithFrame:CGRectMake(100, 50, 100, 100)];
+    [self.view addSubview:_circleProgressView];
     
     WaverView *waverView = [[WaverView alloc] initWithFrame:CGRectMake(0, CGRectGetHeight(self.view.bounds)/2.0 - 50.0, CGRectGetWidth(self.view.bounds), 100.0)];
     //定制
@@ -49,20 +56,58 @@ static float percent = 0;
     [self.view addSubview:waverView];
     // Do any additional setup after loading the view, typically from a nib.
     
-    _circleProgressView = [[CircleProgressView alloc]initWithFrame:CGRectMake(100, 50, 100, 100)];
-    [self.view addSubview:_circleProgressView];
+    UIButton *startRecButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [startRecButton setFrame:(CGRect){20, 240 + 150, 80, 30}];
+    [startRecButton setTitle:@"开始录音" forState:UIControlStateNormal];
+    [startRecButton addTarget:self action:@selector(startRecButtonDidClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:startRecButton];
+    
+    UIButton *stopRecButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [stopRecButton setFrame:(CGRect){150, 240 + 150, 80, 30}];
+    [stopRecButton setTitle:@"结束录音" forState:UIControlStateNormal];
+    [stopRecButton addTarget:self action:@selector(stopRecButtonDidClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:stopRecButton];
+    
+    UIButton *playRecButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [playRecButton setFrame:(CGRect){20, 240 + 200, 80, 30}];
+    [playRecButton setTitle:@"播放录音" forState:UIControlStateNormal];
+    [playRecButton addTarget:self action:@selector(playRecButtonDidClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:playRecButton];
+    
+    UIButton *stopPlayButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [stopPlayButton setFrame:(CGRect){150, 240 + 200, 80, 30}];
+    [stopPlayButton setTitle:@"停止播放" forState:UIControlStateNormal];
+    [stopPlayButton addTarget:self action:@selector(stopPlayButtonDidClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:stopPlayButton];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-    
-    [_kefuClient startRecWithPid:_userInfoDic];
     
     do {
         [_circleProgressView setPercent:percent animated:YES];
         percent += 0.1;
     } while (percent<=100);
     percent = 0;
+}
+
+- (IBAction)startRecButtonDidClick:(id)sender {
+    [_kefuClient startRecWithPid:_userInfoDic];
+}
+
+- (IBAction)stopRecButtonDidClick:(id)sender {
+    [_kefuClient finishSpeak];
+}
+
+- (IBAction)playRecButtonDidClick:(id)sender {
+    NSString *kefuClientPath = [[KMFileManager shareManager] createFilePath];
+    
+    NSString *keyPath = [kefuClientPath stringByAppendingPathComponent:[NSString stringWithFormat:@"%@",[NSString stringWithFormat:@"%@.mp3", _userInfoDic[@"id"]]]];
+    [_mp3Player PlayWithContentFile:keyPath];
+}
+
+- (IBAction)stopPlayButtonDidClick:(id)sender {
+    [_mp3Player stopPlaying];
 }
 
 - (void)setupRecorder  {
